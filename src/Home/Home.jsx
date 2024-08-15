c3b52792d2906e312e15546528755ad79c42bf8a
unknown <abdulbaset.ayon@gmail.com>
2024-08-15 23:37:42 +0600

    made the home page responsive

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -17,14 +17,14 @@ const Home = () => {
     const fetchProducts = async () => {
       try {
         const response = await axios.get("http://localhost:5000/allProducts", {
-          params: { 
-            page: currentPage, 
+          params: {
+            page: currentPage,
             limit,
             searchTerm,
             brand: brandFilter,
             category: categoryFilter,
             priceRange: priceRangeFilter,
-            sort: sortOption
+            sort: sortOption,
           },
         });
 
@@ -36,7 +36,14 @@ const Home = () => {
     };
 
     fetchProducts();
-  }, [currentPage, searchTerm, brandFilter, categoryFilter, priceRangeFilter, sortOption]);
+  }, [
+    currentPage,
+    searchTerm,
+    brandFilter,
+    categoryFilter,
+    priceRangeFilter,
+    sortOption,
+  ]);
 
   const handlePageChange = (page) => {
     if (page > 0 && page <= totalPages) {
@@ -47,26 +54,30 @@ const Home = () => {
   return (
     <div>
       <div className="bg-black">
-        <h1 className="text-4xl p-14 font-normal text-center mb-10 text-white custom-font">
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl p-6 lg:p-14 font-normal text-center mb-6 lg:mb-10 text-white custom-font">
           Our Premium <span className="text-customPurple">Products</span>
         </h1>
       </div>
 
-      <div className="flex lg:flex-row flex-col">
-        <div className="w-1/5">
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:w-1/5 p-4">
           {/* Filter and Sorting */}
-          <div className="p-4 mt-2 rounded-lg custom-font">
+          <div className="mt-2 rounded-lg custom-font">
             <div className="flex py-2 rounded-2xl border border-slate-400 justify-center mb-4">
-              <h2 className="text-2xl text-customPurple custom-font font-bold">Filter</h2>
+              <h2 className="text-xl sm:text-2xl text-customPurple custom-font font-bold">
+                Filter
+              </h2>
             </div>
             <div className="mb-4">
-              <label className="block text-customPurple font-semibold mb-2">Brand</label>
+              <label className="block text-customPurple font-semibold mb-2">
+                Brand
+              </label>
               <select
                 value={brandFilter}
                 onChange={(e) => setBrandFilter(e.target.value)}
-                className="w-full p-2 border rounded-lg"
+                className="w-full p-2 border-l-2 border-customPurple"
               >
-                <option value="">All Brands</option>
+               <option value="">All Brands</option>
                 {/* Add more brand options here */}
                 <option value="Apple">Apple</option>
                 <option value="Samsung">Samsung</option>
@@ -89,16 +100,19 @@ const Home = () => {
                 <option value="Bose">Bose</option>
                 <option value="Dell">Dell</option>
                 <option value="Canon">Canon</option>
+
               </select>
             </div>
             <div className="mb-4">
-              <label className="block text-customPurple font-semibold mb-2">Category</label>
+              <label className="block text-customPurple font-semibold mb-2">
+                Category
+              </label>
               <select
                 value={categoryFilter}
                 onChange={(e) => setCategoryFilter(e.target.value)}
-                className="w-full p-2 border rounded-lg"
+                className="w-full p-2 border-l-2 border-customPurple"
               >
-                <option value="">All Categories</option>
+               <option value="">All Categories</option>
                 <option value="Smartphones">Smartphones</option>
                 <option value="Laptops">Laptops</option>
                 <option value="Tablets">Tablets</option>
@@ -113,16 +127,16 @@ const Home = () => {
                 <option value="Smart Home">Smart Home</option>
                 <option value="Home Appliances">Home Appliances</option>
                 <option value="Accessories">Accessories</option>
-
-
               </select>
             </div>
             <div className="mb-4">
-              <label className="block text-customPurple font-semibold mb-2">Price Range</label>
+              <label className="block text-customPurple font-semibold mb-2">
+                Price Range
+              </label>
               <select
                 value={priceRangeFilter}
                 onChange={(e) => setPriceRangeFilter(e.target.value)}
-                className="w-full p-2 border rounded-lg"
+                className="w-full p-2 border-l-2 border-customPurple"
               >
                 <option value="">All Prices</option>
                 <option value="low">Below $50</option>
@@ -131,11 +145,13 @@ const Home = () => {
               </select>
             </div>
             <div className="mb-4">
-              <label className="block text-customPurple font-semibold mb-2">Sort By</label>
+              <label className="block text-customPurple font-semibold mb-2">
+                Sort By
+              </label>
               <select
                 value={sortOption}
                 onChange={(e) => setSortOption(e.target.value)}
-                className="w-full p-2 border rounded-lg"
+                className="w-full p-2 border-l-2 border-customPurple"
               >
                 <option value="">No Sorting</option>
                 <option value="priceLowToHigh">Price: Low to High</option>
@@ -147,17 +163,17 @@ const Home = () => {
         </div>
 
         {/* Products Display */}
-        <div className="container mx-auto w-4/5 p-6">
-          <div className="container mx-auto mb-6">
+        <div className="container mx-auto lg:w-4/5 p-4 sm:p-6">
+          <div className="container mx-auto mb-4 sm:mb-6">
             <input
               type="text"
               placeholder="Search for products..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full p-3 border rounded-lg shadow-sm focus:outline-none focus:ring focus:border-customPurple"
+              className="w-full p-2 sm:p-3 border-2 rounded-lg shadow-sm border-customPurple focus:outline-none focus:ring focus:border-customPurple"
             />
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-10">
             {products.map((product) => (
               <div
                 key={product._id}
@@ -165,16 +181,16 @@ const Home = () => {
               >
                 <div className="relative">
                   <img
-                    className="w-full h-56 object-cover"
+                    className="w-full h-40 sm:h-56 object-cover"
                     src={product.image}
                     alt={product.name}
                   />
                   <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-40"></div>
-                  <h2 className="absolute bottom-0 left-0 p-4 text-2xl font-semibold text-white">
+                  <h2 className="absolute bottom-0 left-0 p-4 text-xl sm:text-2xl font-semibold text-white">
                     {product.name}
                   </h2>
                 </div>
-                <div className="p-6">
+                <div className="p-4 sm:p-6">
                   <p className="text-gray-600 mb-4">{product.description}</p>
                   <div className="flex items-center justify-between text-gray-800 mb-3">
                     <div className="flex items-center">
@@ -200,11 +216,11 @@ const Home = () => {
             ))}
           </div>
           {/* Pagination Controls */}
-          <div className="flex custom-font justify-center mt-10">
+          <div className="flex custom-font justify-center mt-6 sm:mt-10">
             <button
               onClick={() => handlePageChange(currentPage - 1)}
               disabled={currentPage === 1}
-              className="px-2 py-1 mx-1 text-white bg-customPurple rounded-lg hover:bg-indigo-500 disabled:bg-gray-400"
+              className="px-2 sm:px-4 py-1 sm:py-2 mx-1 text-white bg-customPurple rounded-lg hover:bg-indigo-500 disabled:bg-gray-400"
             >
               Previous
             </button>
@@ -212,7 +228,7 @@ const Home = () => {
               <button
                 key={i}
                 onClick={() => handlePageChange(i + 1)}
-                className={`px-4 py-2 mx-1 rounded-lg ${
+                className={`px-2 sm:px-4 py-1 sm:py-2 mx-1 rounded-lg ${
                   currentPage === i + 1
                     ? "bg-customPurple text-white"
                     : "bg-gray-300 text-gray-800"
@@ -224,7 +240,7 @@ const Home = () => {
             <button
               onClick={() => handlePageChange(currentPage + 1)}
               disabled={currentPage === totalPages}
-              className="px-2 py-1 mx-1 text-white bg-customPurple rounded-lg hover:bg-indigo-500 disabled:bg-gray-400"
+              className="px-2 sm:px-4 py-1 sm:py-2 mx-1 text-white bg-customPurple rounded-lg hover:bg-indigo-500 disabled:bg-gray-400"
             >
               Next
             </button>
